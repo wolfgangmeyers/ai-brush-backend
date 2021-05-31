c6124d0fe4ac5a96f322e45163367b4b54f59477
wolfgangmeyers <wolfgangmeyers@gmail.com>
2021-05-30 19:57:35 -0700

    Working database scan I think

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-import express from "express"
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+const express = require("express")
+
+const converter = AWS.DynamoDB.Converter
 
 // @type: express.Application
 const app = express()
 
-app.get("/jobs", (req, res) => {
-    res.status(200).send({jobs: []})
+app.get("/jobs", async (req, res) => {
+    try {
+        let data = await ddb.scan({
+            TableName: "jobs"
+        }).promise();
+        const jobs = data.Items.map(item => converter.unmarshall(item))
+        res.status(200).send({ jobs: [] })
+    } catch (err) {
+        res.status(400).send({"message": "Didn't work"})
+    };
 })
 
 module.exports = app
