d092555cf86f23f6388a3636247e1e4317b7ba42
wolfgangmeyers <wolfgangmeyers@gmail.com>
2021-05-30 19:14:08 -0700

    Trying api gateway integration

--- ai-brush-backend.js
+++ ai-brush-backend.js
@@ -1,4 +1,30 @@
 exports.handler = async (event) => {
     console.log(event)
-    return {"message": "Successfully executed"};
+
+    /**
+     * 
+     * Access-Control-Allow-Origin: * |
+  allowed domains for CORS access
+Access-Control-Allow-Methods: * |
+  allowed HTTP methods for CORS
+Access-Control-Allow-Headers: * |
+  request headers client can send
+Access-Control-Expose-Headers: 
+  *,x-amzn-remapped-authorization |
+  response headers client can read
+     */
+
+
+    return {
+        "isBase64Encoded": false,
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*",
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Expose-Headers": "*,x-amzn-remapped-authorization"
+        },
+        body: JSON.stringify({})
+    };
 };
