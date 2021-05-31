e3539450aa634ff845ff7b9ade4a2ada75c51da3
wolfgangmeyers <wolfgangmeyers@gmail.com>
2021-05-30 20:18:54 -0700

    Saving for now, too tired

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 const express = require("express")
+const uuid = require("uuid")
 
 const converter = AWS.DynamoDB.Converter
+const apiKey = process.env.API_KEY
 
 // @type: express.Application
 const app = express()
+app.use(express.json())
 
 app.get("/jobs", async (req, res) => {
     try {
@@ -17,6 +20,14 @@ app.get("/jobs", async (req, res) => {
     } catch (err) {
         res.status(400).send({"message": "Didn't work"})
     };
+})
+
+app.post("/jobs", async (req, res) => {
+    const job = req.body
+    console.log(job)
+    const id = uuid.v4()
+    job.id = id
+    res.status(201).send(job)
 })
 
 module.exports = app
